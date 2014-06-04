dbaedd6c210435b6be923b2323c8a55afc100895
SathishAndroidiOS <skd@usaweb.net>
2014-06-04 19:30:12 +0530

    Sathish5

--- ScreenFlashEffect/AppDelegate.h
+++ ScreenFlashEffect/AppDelegate.h
@@ -11,9 +11,7 @@
 @class ViewController;
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
-{
-    
-}
+
 @property (strong, nonatomic) UIWindow *window;
 
 @property (strong, nonatomic) ViewController *viewController;
